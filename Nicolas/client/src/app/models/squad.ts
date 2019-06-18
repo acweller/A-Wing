d492ad42cb05bf584bc12e934beb096a036c63a1
Unknown <milholi51@gmail.com>
2019-06-18 17:54:53 -0300

    Crud + SQL - Squad

---
+++ Nicolas/client/src/app/models/squad.ts
@@ -0,0 +1,6 @@
+export interface Squad {
+    cod_esquadrao?: number; 
+    nom_esquadrao?: string;
+    descricao?: string;
+    datahora?: Date;
+}
---
+++ Nicolas/server/src/keys.ts
@@ -0,0 +1,12 @@
+export default {
+
+    database: {
+       host: 'localhost',
+       user: 'root',
+       password: '',
+       database: 'squadleds',
+       //socket: '/xampp/mysql/mysql.sock',
+    }
+ 
+ }
+ 
