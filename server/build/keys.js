3324a59ea9021a2fcace185e5f8a00e8c12b3835
Anderson Weller <acweller@gmail.com>
2019-06-20 17:16:02 -0300

    Atualização da chave de conexao

--- server/build/keys.js
+++ server/build/keys.js
@@ -3,10 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
         host: 'localhost',
-        user: 'root',
-        //password: '013',
-        //database: 'ng_games_db',
+        user: 'phpmyadmin',
+        password: 'phpmyadmin',
         database: 'db_awing',
-        socket: '/opt/lampp/var/mysql/mysql.sock',
     }
 };
--- server/src/keys.ts
+++ server/src/keys.ts
@@ -2,11 +2,10 @@ export default {
 
    database: {
       host: 'localhost',
-      user: 'root',
-      //password: '013',
-      //database: 'ng_games_db',
+      user: 'phpmyadmin',
+      password: 'phpmyadmin',
       database: 'db_awing',
-      socket: '/opt/lampp/var/mysql/mysql.sock',
+      //socket: '/opt/lampp/var/mysql/mysql.sock',
    }
 
 }
