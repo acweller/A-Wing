3556f44381bc54f939560e0cf891a7b35420ad42
Anderson Weller <acweller@gmail.com>
2019-06-20 23:11:18 -0300

    Arquivos de SRC TS

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -1,11 +1,12 @@
 export default {
 
-   database: {
-      host: 'localhost',
+    database: {
+       host: 'localhost',
       user: 'phpmyadmin',
       password: 'phpmyadmin',
       database: 'db_awing',
       //socket: '/opt/lampp/var/mysql/mysql.sock',
-   }
-
-}
+    }
+ 
+ }
+ 
