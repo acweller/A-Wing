f339b2baa051b3bbb5cd3aec457fff6cb6fe283f
Anderson Weller <acweller@gmail.com>
2019-08-27 18:04:15 -0300

    Detalhamento sobre implantação no Servidor
    Detalhamento sobre implantação no Servidor (ledsst-web01.cefetes.br)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api_uri: 'http://192.168.0.113:3000/api'
+  api_uri: 'http://172.16.174.48:3000/api',
+  apiuri_anderson: 'http://192.168.0.113:3000/api'
 };
