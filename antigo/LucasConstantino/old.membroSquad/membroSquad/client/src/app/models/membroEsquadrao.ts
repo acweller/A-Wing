0413c7d51b4311c72679dc90d7678a0809795195
Anderson Weller <acweller@gmail.com>
2019-08-28 12:02:59 -0300

    Exclusão dos arquivos antigos
    Exclusão dos arquivos antigos e inclusão de parâmetro no TS que impede a geração de JS caso o compilador TS detecte algum erro.

--- antigo/LucasConstantino/old.membroSquad/membroSquad/client/src/app/models/membroEsquadrao.ts
+++
@@ -1,7 +0,0 @@
-
-export interface membroEsquadrao{
-    cod_personagem?: number;
-    cod_esquadrao?: number;
-    descricao?: String;
-    datahora?: Date;
-}
--- antigo/RicardoHNeto/server/build/controllers/indexController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class IndexController {
-    index(req, res) {
-        res.send('Bem-vindo a este super website... Va para localhost:3000/personagem para uma incrivel experiencia geik');
-    }
-}
-exports.indexController = new IndexController();
